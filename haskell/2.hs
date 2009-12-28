d89b4252138ddec57750a3a2d907ccd0f3ac41e9
Kevin Clark <kevin.clark@gmail.com>
2009-12-28 02:17:48 -0500

    Even valued _terms_ not even valued indicies

--- haskell/2.hs
+++ haskell/2.hs
@@ -10,4 +10,5 @@ fib 0 = 1
 fib 1 = 1
 fib x = fib(x - 1) + fib(x - 2)
 
-main = putStr $ show $ sum $ takeWhile (< 4000000) $ map fib [2,4..]
+main = putStr $ show $ sum $ filter even $ fibsUnderFourMil
+    where fibsUnderFourMil = takeWhile (< 4000000) $ map fib [1..]
