cb87167fc99083617790ff28cf7737dff6abdbcc
Kevin Clark <kevin.clark@gmail.com>
2009-12-27 22:43:26 -0500

    Use map instead of a list comprehension

--- haskell/2.hs
+++ haskell/2.hs
@@ -1,5 +1,7 @@
+fib :: Integer -> Integer
+
 fib 0 = 1
 fib 1 = 1
-fib x = fib(x - 1) + fib (x - 2)
+fib x = fib(x - 1) + fib(x - 2)
 
-main = putStr $ show $ sum $ takeWhile (< 4000000) [ fib x | x <- [2,4..]]
+main = putStr $ show $ sum $ takeWhile (< 4000000) $ map fib [2,4..]
