c5f5a8178716a81ee03a705f0590d142a0a20d33
Kevin Clark <kevin.clark@gmail.com>
2009-12-28 13:10:59 -0500

    Add description to solution 7

--- haskell/7.hs
+++ haskell/7.hs
@@ -1,3 +1,7 @@
+--By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6^(th) prime is 13.
+
+--What is the 10001^(st) prime number?
+
 isPrime :: Integer -> Bool
 isPrime x
     | x == 2 = True
